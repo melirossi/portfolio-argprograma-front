6f5ec445d79a82952bea0e39e3ed23f7bc2eaee5
Melisa <melisa.s.rossi@gmail.com>
2023-04-21 18:11:17 -0300

    modificación cors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent}, 
   {path:'login', component: LoginComponent},
-  {path:'nuevaexp', component: NewExperienciaComponent}
+  {path:'nuevaexp', component: NewExperienciaComponent},
+  {path:'editexp/:id', component: EditExperienciaComponent}
 ];
 
 @NgModule({
--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -30,13 +30,16 @@ export class ExperienciaComponent implements OnInit {
 
   delete(id?: number){
     if(id != undefined){
-      this.sExperiencia.delete(id).subscribe(
-        data => {
+      this.sExperiencia.delete(id).subscribe({
+        next: (data) => {
           this.cargarExperiencia();
-        }, err => {
+        },
+        error: (err) => {
           alert("No se pudo borrar la experiencia");
         }
-      )
+      });       
     }
   }
 }
+
+
